e0faadf57b8d96c069434e59907b78d87c4e413e
Mitori <3098805300@qq.com>
2026-05-16 10:24:43 +0800

    fix: shuzu

--- custom-backend/services/likelist.js
+++ custom-backend/services/likelist.js
@@ -1,24 +1,35 @@
-async function syncNcmLikelist(userid, supabaseAdmin,likelist){ 
-  console.log('syncNcmLikelist called with userid:', userid, 'likelist:', likelist)
-  
-  const ret = likelist.map(async item=>{
-    const { data, error } = await supabaseAdmin.from('like_list')
-      .upsert({
-        user_id: userid,
-        song_id: item,
-      }
-      , { onConflict: 'user_id,song_id' })
+async function syncNcmLikelist(userid, supabaseAdmin, likelist) {
+  console.log(
+    'syncNcmLikelist called with userid:',
+    userid,
+    'likelist:',
+    likelist,
+  )
+
+  const ret = likelist.map(async (item) => {
+    const { data, error } = await supabaseAdmin
+      .from('like_list')
+      .upsert(
+        {
+          user_id: userid,
+          song_id: item,
+        },
+        { onConflict: 'user_id,song_id' },
+      )
       .select('song_id')
 
     if (error) {
       console.error('Error syncing like list item:', item, 'Error:', error)
       throw error
     }
-    console.log(data);
-    return data.song_id;
+    console.log(data)
+
+    const songId = data?.[0]?.song_id
+
+    return songId
   })
 
-  return await Promise.all(ret) ?? [];
+  return (await Promise.all(ret)) ?? []
 }
 
 module.exports = {
