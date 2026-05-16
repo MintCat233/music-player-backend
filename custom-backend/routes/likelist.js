9219ff58cb7d17fcfb756e232e8c1c1c6445dbf8
Mitori <3098805300@qq.com>
2026-05-16 10:33:36 +0800

    feat: getlikelist

--- custom-backend/routes/likelist.js
+++ custom-backend/routes/likelist.js
@@ -3,7 +3,10 @@ const express = require('express')
 const { createSupabaseAdminClient } = require('../lib/supabase')
 const { createAppAuthMiddleware } = require('../middleware/auth')
 const { sendError, sendSuccess } = require('../util/response')
-const{ syncNcmLikelist } = require('../services/likelist')
+const{ syncNcmLikelist,
+  getLikelist
+
+ } = require('../services/likelist')
 
 function createLikelistRouter(config) {
   const requireAuth = createAppAuthMiddleware(config.jwt)
@@ -36,7 +39,23 @@ function createLikelistRouter(config) {
     }
   })
 
+  router.post('/',requireAuth, async (req, res) => {
+    const body = req.body || {}
+    const userid = body.userid || null
+    
+    if (!userid) {
+      sendError(res, 400, 'userid is required')
+      return
+    }
+    const result=await getLikelist(userid,supabaseAdmin)
+    sendSuccess(res, {
+      likelist: result,
+    })
+  })
+
   return router
+
+  
 }
 
 module.exports = {
